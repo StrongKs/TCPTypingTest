43429108a79537b8ef3aed1d8cfb74cb2ad34718
kentphipps <kentphipps99@gmail.com>
2024-04-20 13:26:59 -0400

    First commit. Foundation of TypingTest. Messages sent to and from client/server.

---
+++ README.md
@@ -0,0 +1,32 @@
+# Project Assignment 3
+
+### Student Information
+1. Kent Phipps    UFID: 402092184
+2. Waleed
+1. Samir
+
+### How to compile and run
+*Assuming you have Client and Server file in different machines*
+1. Compile and run server.java
+
+    • Open a terminal at Server folder and execute: 
+
+        javac Server.java
+        java Server <port>
+
+2. Comple and run client.java
+
+    • Open a terminal at Client folder and execute:
+
+        javac Client.java
+        java Client <Server-ip> <port>
+
+3. Execute commands "Joke 1", "Joke 2", or "Joke 3" from the client terminal to recieve joke.txt files.
+
+        Joke 1
+        Joke 2
+        Joke 3
+
+4. Execute command "bye" to gracefully exit the cilent-server connection
+
+        bye
---
+++ Server/Server.java
@@ -0,0 +1,81 @@
+// A Java program for a Server
+import java.net.*;
+
+import javax.xml.crypto.Data;
+
+import java.io.*;
+
+public class Server
+{
+	//initialize socket and input stream
+	private Socket		 socket = null;
+	private ServerSocket server = null;
+	private DataInputStream in	 = null;
+	private DataOutputStream out = null;
+
+	// constructor with port
+	public Server(int port)
+	{
+		// starts server and waits for a connection
+		try
+		{
+			server = new ServerSocket(port);
+			System.out.println("Server started");
+
+			System.out.println("Waiting for a client ...");
+
+			socket = server.accept();
+			System.out.println("Client accepted");
+
+			// takes input from the client socket
+			in = new DataInputStream(
+				new BufferedInputStream(socket.getInputStream()));
+
+			// sends output to the socket
+			out = new DataOutputStream(socket.getOutputStream());
+
+			String line = "";
+
+			// send instructions to client
+			try {
+				out.writeUTF("Type 'Ready' to start Typing test. Type 'Over' to end the connection");
+				out.flush();
+			} catch (IOException e) {
+				System.out.println(e);
+			}
+
+			// reads message from client until "Over" is sent
+			while (!line.equals("Over"))
+			{
+				try
+				{
+					line = in.readUTF();
+					System.out.println("Client: " + line);
+
+					// write to the client
+					out.writeUTF("thank you for the message: " + line);
+					out.flush();
+
+				}
+				catch(IOException i)
+				{
+					System.out.println(i);
+				}
+			}
+			System.out.println("Closing connection");
+
+			// close connection
+			socket.close();
+			in.close();
+		}
+		catch(IOException i)
+		{
+			System.out.println(i);
+		}
+	}
+
+	public static void main(String args[])
+	{
+		Server server = new Server(4444);
+	}
+}
